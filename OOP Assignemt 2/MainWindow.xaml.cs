97d1da6640a0fade0ea459efca83446f1a39df55
DeanAhmed <145498673+DeanAhmed@users.noreply.github.com>
2023-09-19 20:40:38 +0100

    Add files via upload

---
+++ OOP Assignemt 2/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BIZ;
+using DAL;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace OOP_Assignemt_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Hash h = new Hash();
+        DAO d = new DAO();
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = h.HashData(txtPass.Password);
+
+            SqlCommand cmd = d.OpenCon().CreateCommand();
+            cmd.CommandText = "uspLogin";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@user", user);
+            cmd.Parameters.AddWithValue("@pass", pass);
+
+            SqlDataReader dr = null;
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("Succesfully Logged in");
+                lblPass.IsEnabled = false;
+                lblUser.IsEnabled = false;
+                txtPass.IsEnabled = false;
+                txtUser.IsEnabled = false;
+                btnLogin.IsEnabled = false;
+                btnReg.IsEnabled = false;
+                txtUser.Clear();
+                txtPass.Clear();
+                msFile.IsEnabled = true;
+                msLoginExit.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Login");
+                txtUser.Clear();
+                txtPass.Clear();
+            }
+            d.CloseCon();
+        }
+
+        private void btnReg_Click(object sender, RoutedEventArgs e)
+        {
+            Register r = new Register();
+            r.Show();
+        }
+    }
+}
---
+++ OOP Assignemt 2/Register.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DAL;
+using BIZ;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OOP_Assignemt_2
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+        DAO d = new DAO();
+        Hash h = new Hash();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = h.HashData(txtPass.Password);
+
+            SqlCommand cmd = d.OpenCon().CreateCommand();
+            cmd.CommandText = "uspRegister";
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@user", user);
+            cmd.Parameters.AddWithValue("@pass", pass);
+
+            cmd.ExecuteNonQuery();
+            d.CloseCon();
+
+            txtUser.Clear();
+            txtPass.Clear();
+        }
+    }
+}
